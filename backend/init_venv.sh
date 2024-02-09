ddce9db67cdbda66327f959cf423e7fbb8742bb7
Diogo Pereira <diogo.pereira.ee@gmail.com>
2024-02-09 15:11:18 +0000

    Added a functional test of login used django.contrib.auth. Updated notes with new info.

--- backend/init_venv.sh
+++ backend/init_venv.sh
@@ -6,7 +6,7 @@ python3 -m venv venv
 # activate venv
 source venv/bin/activate
 
-pip3 install --upgrade pip3
+pip3 install --upgrade pip
 
 # install dependencies of project
 pip3 install -r requirements.txt
