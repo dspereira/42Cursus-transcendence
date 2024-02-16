00d5fc0369355905b3a6d22a2d371950bb7bf8df
dspereira <diogo.pereira.ee@gmail.com>
2024-02-16 17:23:36 +0000

    Updated Cors middleware. Already works.

--- backend/transcendence/core_middlewares/Cors.py
+++ backend/transcendence/core_middlewares/Cors.py
@@ -1,5 +1,23 @@
 
-# Configurations of cors
+# Configurations of cors headers
+
+origin_name = "Access-Control-Allow-Origin"
+origin_data = [
+    "http://127.0.0.1:8080",
+]
+
+headers_name = "Access-Control-Allow-Headers"
+headers_data = [
+    "Content-Type",
+]
+
+methods_name = "Access-Control-Allow-Methods"
+methods_data = [
+    "GET",
+    "POST",
+    "DELETE",
+]
+
 
 class Cors:
 
@@ -7,15 +25,29 @@ class Cors:
         self.get_response = get_response
 
     def __call__(self, request):
-        return response_process(self.get_response(request))
+        return process_response(self.get_response(request))
 
+def process_response(response):
 
-def response_process(response):
+    #response["Access-Control-Allow-Origin"] = "*"
+    #response["Access-Control-Allow-Headers"] = "Content-Type"
+    #response["Access-Control-Allow-Methods"] = "PUT, PATCH, DELETE"
 
-    response["Access-Control-Allow-Origin"] = "*"
-    response["Access-Control-Allow-Headers"] = "Content-Type"
-    response["Access-Control-Allow-Methods"] = "PUT, PATCH, DELETE"
-
-    print(response["Access-Control-Allow-Origin"])
+    create_header(response, origin_name, origin_data)
+    create_header(response, headers_name, headers_data)
+    create_header(response, methods_name, methods_data)
 
     return response
+
+def create_header(response, name, data):
+
+    response[name] = ""
+    list_len = len(data)
+    if (list_len == 0):
+        return response
+    for idx, item in enumerate(data):
+        if (idx < list_len - 1):
+            item += ", "
+        response[name] += item
+    return response
+    
