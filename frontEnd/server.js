7dcb471bb01e0544881f87caad46655ad6c8d0fc
pcoimbra <pcoimbra@pcoimbra.pcoimbra>
2024-03-29 08:25:36 -1100

    testing images, server changed to try and fix images being imported as text, but not working so far

--- frontEnd/server.js
+++ frontEnd/server.js
@@ -3,7 +3,32 @@ const path = require("path");
 
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+// app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static"), {
+    setHeaders: (res, filePath) => {
+        const contentType = getFileContentType(filePath);
+        if (contentType) {
+            res.set("Content-Type", contentType);
+        }
+    }
+}));
+
+// Function to determine the Content-Type header based on file extension
+function getFileContentType(filePath) {
+    const extension = path.extname(filePath).toLowerCase();
+    switch (extension) {
+        case ".png":
+            return "image/png";
+        case ".jpg":
+        case ".jpeg":
+            return "image/jpeg";
+        case ".gif":
+            return "image/gif";
+        default:
+            return null;
+    }
+}
 
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
