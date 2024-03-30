2e98f31e06160e240712f0c0c20fb512b08a3bec
pcoimbra <pcoimbra@pcoimbra.pcoimbra>
2024-03-30 01:28:53 -1100

    fixed navbar at top and understood why good practice demands people to keep their own css files. /Posts has 2000 words from lorem to test the navbar out!

--- frontEnd/frontend/static/js/views/Profile.js
+++ frontEnd/frontend/static/js/views/Profile.js
@@ -13,7 +13,6 @@ export default class extends Aview {
 			<h1>This Your account page!</h1>
 
 			look how pretty you look:
-			<img src="../../../images/bobTheBuilder.png" alt="YourPhoto" width="500" height="600"> 
 
 			<h1 class="fw-bold">Under construction!</h1>
 			<p><b><a href="/" data-link>Home.</a></b></p>
