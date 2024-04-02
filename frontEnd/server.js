88beeeabc646a53884618acbdb19a7e7fd1cc649
Pedro <93326846+PedroCVera@users.noreply.github.com>
2024-04-02 14:16:35 +0100

    Update server.js
    removed comments

--- frontEnd/server.js
+++ frontEnd/server.js
@@ -7,31 +7,6 @@ const app = express();
 
 app.use("/", express.static(path.resolve(__dirname, "frontend")));
 
-// app.use("/static", express.static(path.resolve(__dirname, "frontend", "static"), {
-//     setHeaders: (res, filePath) => {
-//         const contentType = getFileContentType(filePath);
-//         if (contentType) {
-//             res.set("Content-Type", contentType);
-//         }
-//     }
-// }));
-
-// // Function to determine the Content-Type header based on file extension
-// function getFileContentType(filePath) {
-//     const extension = path.extname(filePath).toLowerCase();
-//     switch (extension) {
-//         case ".png":
-//             return "image/png";
-//         case ".jpg":
-//         case ".jpeg":
-//             return "image/jpeg";
-//         case ".gif":
-//             return "image/gif";
-//         default:
-//             return null;
-//     }
-// }
-
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
