eeac334fbe3294b35dbf226ee526e925594a3842
dspereira <diogo.pereira.ee@gmail.com>
2024-04-04 12:30:26 +0100

    Modified the chat consumers to close connections when users log out or when access tokens expire.

--- backend/transcendence/live_chat/auth_utils.py
+++ backend/transcendence/live_chat/auth_utils.py
@@ -1,11 +1,20 @@
+from datetime import datetime
 from custom_utils.jwt_utils import JwtData
 from custom_utils.models_utils import ModelManager
 from user_auth.models import BlacklistToken
+from user_auth.models import User
 
-def is_authenticated(token):
-	access_data = JwtData(token)		
+def is_authenticated(access_data):
 	if access_data:
-		blacklist_model = ModelManager(BlacklistToken)
-		if not blacklist_model.get(jti=access_data.jti):
-			return True
+		if access_data.exp >= int(datetime.now().timestamp()):
+			blacklist_model = ModelManager(BlacklistToken)
+			if not blacklist_model.get(jti=access_data.jti):
+				return True
 	return False
+
+def get_authenticated_user(user_id):
+	user_model = ModelManager(User)
+	user = user_model.get(id=user_id)
+	if user:
+		return user
+	return None
--- backend/transcendence/live_chat/consumers.py
+++ backend/transcendence/live_chat/consumers.py
@@ -10,25 +10,27 @@ from .models import ChatRoom, Message
 
 from channels.exceptions import StopConsumer
 
-from .auth_utils import is_authenticated
+from .auth_utils import is_authenticated, get_authenticated_user
 
 
 class ChatConsumer(WebsocketConsumer):
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(args, kwargs)
+		self.user = None
+		self.access_data = None
+		
 
 	def connect(self):
 
-		self.accept()
-		if not self.__is_user_authenticated():
+		self.accept()		
+		self.access_data = self.scope['access_data']
+		if is_authenticated(self.access_data):
+			self.user = get_authenticated_user(self.access_data.sub)
+		if not self.user:
 			self.close(4000)
 			return
-
-		self.user = self.__getUser()
-		if not self.user:
-			self.close()
-
+		
 		self.username = self.user.username
 		self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
 		self.room = ChatRoom.objects.get(id=self.room_id)
@@ -98,8 +100,6 @@ class ChatConsumer(WebsocketConsumer):
 		data_json = json.loads(text_data)
 		message = data_json['message'].strip()
 
-		self.__getAccessToken()
-
 		if message:
 			result_message = f"{self.username}: {message}"
 
@@ -123,6 +123,10 @@ class ChatConsumer(WebsocketConsumer):
 
 		#if not is_authenticated(self.__getAccessTokenTest()):
 		#	self.disconnect(4000)
+  
+		if not is_authenticated(self.access_data):
+			self.close(4000)
+			return
 
 		message = event['message']
 		self.send(text_data=json.dumps({
@@ -147,38 +151,4 @@ class ChatConsumer(WebsocketConsumer):
 		}))
 		print(message)
 
-	def __getUser(self):
 
-		user = None
-		access_token = self.__getAccessToken()
-
-		try:
-			user = User.objects.get(id=access_token.sub)
-			if user:
-				return user
-			return None
-		except Exception as e:
-			print(f"WebSocket Error: {e}")
-			return None
-
-	def __getAccessToken(self):
-
-		for header_name, header_value in self.scope['headers']:
-			if header_name == b'cookie':
-				cookies = header_value.decode('utf-8').split('; ')
-				for cookie in cookies:
-					cookie_name, cookie_value = cookie.split('=')
-					if cookie_name == 'access':
-						access_cookie = cookie_value
-						break
-
-		#print("=======================================================")
-		#print(f"Access Token:\n{access_cookie}")
-		#print("=======================================================")
-
-		return JwtData(access_cookie)
-
-	def __is_user_authenticated(self):
-		if self.scope['access_data']:
-			return True
-		return False
