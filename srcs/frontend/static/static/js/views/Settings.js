0730b1ea90d17f3d1a203b2c57b1710a4fa172da
dspereira <diogo.pereira.ee@gmail.com>
2024-04-15 12:34:23 +0100

    Added static files of the web app to nginx docker

---
+++ srcs/frontend/static/static/js/views/Settings.js
@@ -0,0 +1,19 @@
+import Aview from "./Aview.js";
+
+export default class extends Aview {
+	constructor(params)
+	{
+		super(params);
+		this.setTitle("Settings");
+	}
+
+	async getHtml() 
+	{
+		return `
+			<h1>Settings</h1>
+			<p>
+				Configure your settings here!
+			</p>
+		`;
+	}
+}
