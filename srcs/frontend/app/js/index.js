1c36dc46cc563e21a47e7c9d285dd40dec9fc1f7
dspereira <diogo.pereira.ee@gmail.com>
2024-04-22 15:10:40 +0100

    Fixed bugs on the router in index.js. The 'go back' and 'go forward' functionalities are now fully functional.

--- srcs/frontend/app/js/index.js
+++ srcs/frontend/app/js/index.js
@@ -1,8 +1,6 @@
 import PageLogin from "../page-components/page-login.js";
 import PageSignup from "../page-components/page-signup.js";
 
-let routeEvents = [];
-
 const routes = {
 	"/"				: PageLogin.componentName,
 	"/index.html"	: PageLogin.componentName,
@@ -10,70 +8,35 @@ const routes = {
 	"/signup"		: PageSignup.componentName
 }
 
-const removeRouteEvents = function(){
-	routeEvents.forEach(function(elm) {
-		elm.removeEventListener("click", routeLinkEventHandeler);
-	});
-	routeEvents = [];
-}
-
-
-const getPage = function (url) {
-	return routes[url];
-}
-
 const render = function(page) {
 	document.querySelector("#app").innerHTML = `<${page}></${page}>`;
 }
 
-const router = function(newRoute) {
-	const route = newRoute || location.pathname;
-
-	console.log("Page routed");
-	history.pushState({route: route}, null, route);
-	render(getPage(route));
-	removeRouteEvents();
+const router = function() {
+	console.log(`route to ${location.pathname}`);
+	render(routes[location.pathname]);
 	setAllRouteEvents();
 }
 
-const routeLinkEventHandeler = function(event) {
+const routeEventHandler = function(event) {
 	event.preventDefault();
-	router(this.getAttribute('href'));
+	const route = this.getAttribute('href');
+	history.pushState({route: route}, null, route);
+	router();
 }
 
-// adicionar e remover os eventos para não causar problemas de eventos repetidos
 const setAllRouteEvents = function(){
-	const link = document.querySelector('a[href]');
-
-	link.addEventListener('click', routeLinkEventHandeler);
-	routeEvents.push(link)
-
-	console.log(routeEvents);
-	window.onpopstate = function(event)
-	{
-		console.log(`onpopstate: ${event.state.route}`);
-		if (event.state.route)
-			router(event.state.route);
-	}
+	document.querySelector('a[href]').addEventListener('click', routeEventHandler);
 }
 
-
-/*
-const setAllRouteEvents = function(){
-	const links = document.querySelectorAll('a[href]');
-
-	links.forEach(link => {
-		const newRoute = link.innerHTML;
-		link.addEventListener('click', function(event) {
-			event.preventDefault();
-			history.pushState({route: newRoute}, null, newRoute);
-			router();
-			console.log(`new event link ${newRoute}`);
-		});
+const setHistoryEvents = function()
+{
+	window.addEventListener("popstate", (event) => {
+		router();
 	});
 }
-*/
 
 document.addEventListener('DOMContentLoaded', () => {
-	router(location.pathname)
+	router();
+	setHistoryEvents();
 });
