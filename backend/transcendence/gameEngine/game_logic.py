118cd66d2c0f8019cb8eb9eeac5d2f3f8497fd70
pcoimbra <pedro.coimbra.vera@gmail.com>
2024-04-23 16:33:53 +0100

    identation

--- backend/transcendence/gameEngine/game_logic.py
+++ backend/transcendence/gameEngine/game_logic.py
@@ -2,9 +2,9 @@
 from models import Ball, Paddle, GameEngine, GameData
 
 gData = GameData(800, 500, 10,\
-				 10, 20, 5, 20, 0, 15,\
-				 785, 20, 5, 20, 0 , 15,\
-				 400, 250, 1, 0.75, 4, 4, 4)
+				10, 20, 5, 20, 0, 15,\
+				785, 20, 5, 20, 0 , 15,\
+				400, 250, 1, 0.75, 4, 4, 4)
 
 def update_game(data):
 	game = GameEngine(gData)
