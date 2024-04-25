290b22ff58994c49168a7dc1682aa49b0d263534
Diogo13Antunes <diogoandrecandeias@gmail.com>
2024-04-25 14:04:44 +0100

    Changed Password of Email

--- backend/transcendence/transcendence/settings.py
+++ backend/transcendence/transcendence/settings.py
@@ -166,4 +166,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'blitzpong42@gmail.com'
-EMAIL_HOST_PASSWORD = 'vjgffblpyxolqywh'
+EMAIL_HOST_PASSWORD ="jchr ccsy sefv cvxh" 
