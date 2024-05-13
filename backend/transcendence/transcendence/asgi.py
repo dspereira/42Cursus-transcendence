29b9df20dd7ad52b836b457a7ba46fc1cf23b92f
Diogo13Antunes <diogoandrecandeias@gmail.com>
2024-05-13 16:30:08 +0100

    Updated websockets schema in chat.

--- backend/transcendence/transcendence/asgi.py
+++ backend/transcendence/transcendence/asgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from live_chat.routing import websocket_urlpatterns
+from live_chat.routing import chat_urls
 from custom_middlewares import ChannelsAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcendence.settings')
@@ -20,7 +20,7 @@ application = ProtocolTypeRouter({
 	'http': get_asgi_application(),
 	"websocket": AllowedHostsOriginValidator(
 		ChannelsAuthMiddleware(
-			URLRouter(websocket_urlpatterns)
+			URLRouter(chat_urls)
 		),
 	),
 })
