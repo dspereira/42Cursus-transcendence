31cea1afa33038730af2e6e8600ef23ab97a9dcf
dspereira <diogo.pereira.ee@gmail.com>
2024-05-22 20:22:50 +0100

    Updated chat user interface.

--- srcs/frontend/app/components/app-chat.js
+++ srcs/frontend/app/components/app-chat.js
@@ -1,6 +1,7 @@
 
 const styles = `
 
+/* Friend List */
 .red { 
 	border: 1px red solid;
 }
@@ -25,21 +26,52 @@ const styles = `
 	font-weight: bold;
 }
 
+
+/* Chat panel */
+
 .chat-panel .profile-photo {
-	width: 30px;
+	width: 50px;
 }
 
-.chat-panel .message {
-	font-size: 16px;
+.msg {
+	margin-bottom: 20px;	
 }
 
-.sender .message {
-	background-color: blue;
+.card-msg {
+	display: block;
+	max-width: 70%;
 }
 
-.receiver .message {
-	background-color: green;
+.card-text {
+	margin-top: 6px;
+	padding: 5px 8px 5px 8px;
+	border-radius: 8px; 
 }
+
+.friend-color {
+	background-color: #FF5733;
+}
+
+.own-color {
+	background-color: #33FFBD;
+}
+
+
+.friend-pos {
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-start;
+	gap: 15px;
+	
+}
+
+.own-pos {
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-end;
+	gap: 15px;
+}
+
 
 `;
 
@@ -68,20 +100,25 @@ const getHtml = function(data) {
 
 		<div class="col-8">
 			<div class="chat-panel">
-				<div class="sender">
-					<img src="https://api.dicebear.com/8.x/bottts/svg?seed=Diogo" class="profile-photo" alt="profile photo chat"/>
-					<span class="message">Shankle buffalo tongue corned beef shoulder cow chislic hamburger beef sausage burgdoggen. Short ribs beef pork chop, andouille pork ribeye cow burgdoggen chicken pig tenderloin salami leberkas.</span>
-				</div>
-				<div class="receiver">
-					<img src="https://api.dicebear.com/8.x/bottts/svg?seed=Diogo" class="profile-photo" alt="profile photo chat"/>
-					<div class="message">Shankle buffalo tongue corned beef shoulder cow chislic hamburger beef sausage burgdoggen. Short ribs beef pork chop, andouille pork ribeye cow burgdoggen chicken pig tenderloin salami leberkas.</div>
-				</div>
-				<div class="receiver">
-					<img src="https://api.dicebear.com/8.x/bottts/svg?seed=Diogo" class="profile-photo" alt="profile photo chat"/>
-					<div class="message">
-						<span class="message">Shankle buffalo tongue corned beef shoulder cow .</span>
+
+				<div class="msg friend-pos">
+					<div>
+						<img src="https://api.dicebear.com/8.x/bottts/svg?seed=Diogo" class="profile-photo" alt="profile photo chat"/>
+					</div>
+					<div class="card-msg">
+						<div class="friend-color card-text">Bacon ipsum dolor amet bresaola beef tongue, burgdoggen flank brisket ham meatloaf pastrami chislic. Bresaola shoulder alcatra frankfurter leberkas boudin capicola. Meatball buffalo swine cow, ham hock short loin ball tip fatback pancetta landjaeger pork loin kevin drumstick shank. </div>
 					</div>
 				</div>
+
+				<div class="msg own-pos">
+					<div class="card-msg">
+						<div class="own-color card-text">Bacon ipsum dolor amet bresaola beef tongue, burgdoggen flank brisket ham meatloaf pastrami chislic. Bresaola shoulder alcatra frankfurter leberkas boudin capicola. Meatball buffalo swine cow, ham hock short loin ball tip fatback pancetta landjaeger pork loin kevin drumstick shank. </div>
+					</div>
+					<div>
+						<img src="https://api.dicebear.com/8.x/bottts/svg?seed=Diogo" class="profile-photo" alt="profile photo chat"/>
+					</div>
+				</div>				
+
 			</div>
 		</div>
 	</div>
