1f2378f663ba590a392577f4b2677a29f969d369
Diogo13Antunes <diogoandrecandeias@gmail.com>
2024-06-24 16:38:08 +0100

    Improved script to make all migrations.

--- make_all_migrations.sh
+++ make_all_migrations.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+create_files_in_dirs() {
+	local directories=("$@")
+
+	for dir in "${directories[@]}"; do
+		if [ -d "$dir" ]; then
+			cd $dir && mkdir migrations && cd migrations && touch __init__.py && cd ../..
+			echo "Directory migrations created at $dir"
+		else
+			echo "Directory $dir does not exist!"
+		fi
+	done
+}
+
 cd backend || exit
 source ./venv/bin/activate || exit
 
-cd transcendence || exit
+cd ..
+bash delete_all_migrations.sh -y || exit
+
+cd backend/transcendence || exit
+
+dirs=("live_chat/" "two_factor_auth/" "friendships/" "user_profile/" "notifications/" "user_auth/")
+create_files_in_dirs "${dirs[@]}"
+
+pwd
+
 python3 manage.py makemigrations
 python3 manage.py migrate
 
