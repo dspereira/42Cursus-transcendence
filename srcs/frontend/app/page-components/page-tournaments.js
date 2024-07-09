ec696e626d6e855e0da88af3f3977ed7917b1da0
Ricardo Lopes <ricardoalgill@gmail.com>
2024-07-09 19:58:25 +0100

    added a toggle for past/active tournaments + respective pages and made a template card for past tournaments

--- srcs/frontend/app/page-components/page-tournaments.js
+++ srcs/frontend/app/page-components/page-tournaments.js
@@ -2,20 +2,102 @@ import {redirect} from "../js/router.js";
 import { adjustContent } from "../utils/adjustContent.js";
 import stateManager from "../js/StateManager.js";
 
-const styles = ``;
+const styles = `
+
+	.tab-select {
+		display: flex;
+		width: 100%;
+		height: 120px;
+		background-color: #EEEEEE;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.tab-select-btn {
+		display: flex;
+		width: 400px;
+		height: 100px;
+		color: white;
+		background-color: #E0E0E0;
+		border-style: hidden;
+		border-radius: 5px;
+	}
+
+	.select-left, .select-right{
+		display: flex;
+		width: 50%;
+		height: 100%;
+		border-style: hidden;
+		justify-content: center;
+		align-items: center;
+		--toggled: off;
+		font-size: 16px;
+		font-weight: bold;
+	}
+
+	.select-left {
+			border-radius: 5px 0px 0px 5px;
+			background-color: #C2C2C2;
+	}
+
+	.select-right {
+		border-radius: 0px 5px 5px 0px;
+		background-color: #E0E0E0;
+	}
+
+	.active-tournaments, .past-tournaments {
+		width: 100%;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		border-style: hidden;
+		border-radius: 5px;
+	}
+
+	.active-tournaments {
+		display: flex;
+	}
+
+	.past-tournaments {
+		display: none;
+	}
+
+	.past-tournament-card {
+		display: flex;
+		width: 80%;
+		height: 200px;
+		justify-content: space-between;
+		margin: 0px 0px 20px;
+		background-color: #E0E0E0;
+		border-style: hidden;
+		border-radius: 5px;
+	}
+	
+	.card-box {
+		display: flex;
+		width: 150px;
+		height: 80%;
+		justify-content: center;
+		align-items: center;
+		margin-left: 20px;
+		margin-right: 20px;
+	}
+`;
 
 const getHtml = function(data) {
 	const html = `
 		<app-header></app-header>
 		<side-panel selected="tournaments"></side-panel>
 		<div class="content content-small">
-			<h1>Page Tournaments</h1>
-			<p>
-			Rump drumstick tri-tip alcatra. Flank ground round pastrami beef short ribs pork belly jowl. Spare ribs beef ribs andouille, frankfurter short loin shankle venison salami turducken. Beef ribs alcatra capicola shoulder pork loin sirloin biltong turkey pancetta flank pork andouille bacon. Doner hamburger shoulder tenderloin flank prosciutto corned beef. Chislic tongue doner porchetta pastrami sirloin filet mignon leberkas brisket ribeye pork chop shank cupim corned beef sausage.
-			</p>
-			<p>
-			Rump drumstick tri-tip alcatra. Flank ground round pastrami beef short ribs pork belly jowl. Spare ribs beef ribs andouille, frankfurter short loin shankle venison salami turducken. Beef ribs alcatra capicola shoulder pork loin sirloin biltong turkey pancetta flank pork andouille bacon. Doner hamburger shoulder tenderloin flank prosciutto corned beef. Chislic tongue doner porchetta pastrami sirloin filet mignon leberkas brisket ribeye pork chop shank cupim corned beef sausage.
-			</p>
+			<div class="tab-select">
+				<button class="tab-select-btn">
+					<div class="select-left">New/Current<br>tournament</div>
+					<div class="select-right">Past<br>Tournaments</div>
+				</button>
+			</div>
+			
+			<div class="active-tournaments">NEW TOURNAMENTS GO HERE</div>
+			<div class="past-tournaments">PAST TOURNAMENTS GO HERE</div>
 		</div>
 	`;
 	return html;
@@ -67,8 +149,106 @@ export default class PageTournaments extends HTMLElement {
 	}
 
 	#scripts() {
+		const pastTournamentsList = getFakeTournaments();
 		adjustContent(this.html.querySelector(".content"));
+		this.#toggleTabSelector();
+		this.#getPastTournaments(pastTournamentsList);
 	}
-}
 
-customElements.define(PageTournaments.componentName, PageTournaments);
+
+	#toggleTabSelector() {
+		this.html.querySelector(".tab-select-btn").addEventListener("click", () => {
+			const leftSlct = this.html.querySelector(".select-left");
+			const rightSlct = this.html.querySelector(".select-right");
+			const newTournaments = this.html.querySelector(".active-tournaments");
+			const pastTournaments = this.html.querySelector(".past-tournaments");
+			const isToggled = leftSlct.style.getPropertyValue('--toggled') === 'on';
+			leftSlct.style.setProperty('--toggled', isToggled ? 'off' : 'on');
+			if (isToggled) {
+				leftSlct.style.backgroundColor = "#C2C2C2";
+				rightSlct.style.backgroundColor = "#E0E0E0";
+				leftSlct.style.color = "white";
+				rightSlct.style.color = "#C2C2C2";
+				newTournaments.style.display = "flex";
+				pastTournaments.style.display = "none";
+			} else {
+				leftSlct.style.backgroundColor = "#E0E0E0";
+				rightSlct.style.backgroundColor = "#C2C2C2";
+				leftSlct.style.color = "#C2C2C2";
+				rightSlct.style.color = "white";
+				newTournaments.style.display = "none";
+				pastTournaments.style.display = "flex";
+			}
+		});
+	}
+
+	#getPastTournaments(pastTournaments) {
+		const pastTournamentsHtml = this.html.querySelector(".past-tournaments");
+		pastTournaments.forEach((tournament) => {
+			pastTournamentsHtml.appendChild(this.#pastTournamentCard(tournament));
+		});
+	}
+
+	#pastTournamentCard(tournament) {
+		const elm = document.createElement("div");
+		elm.classList.add("past-tournament-card");
+		if (tournament) {
+			elm.innerHTML = `
+				<div class="card-box" style="font-size:24px">${tournament.name}</div>
+				<div class="card-box"">1st: ${tournament.first}<br>2nd: ${tournament.second}<br>3rd: ${tournament.third}<br>4th: ${tournament.fourth}</div>
+				<div class="card-box"">${tournament.date}</div>
+			`;
+		}
+		return elm;
+	}
+
+	}
+	
+	customElements.define(PageTournaments.componentName, PageTournaments);
+	
+	
+const getFakeTournaments = function () {
+	const data = `[
+	{
+		"name": "Jhonny's tournament",
+		"first": "Jhonny",
+		"second": "Clara",
+		"third": "Arthur",
+		"fourth": "Marth",
+		"date": "05/07/24"
+	},
+	{
+		"name": "Summer Bash",
+		"first": "Alice",
+		"second": "Bob",
+		"third": "Charlie",
+		"fourth": "Diana",
+		"date": "06/15/24"
+	},
+	{
+		"name": "Winter Cup",
+		"first": "Eve",
+		"second": "Frank",
+		"third": "Grace",
+		"fourth": "Hank",
+		"date": "12/10/24"
+	},
+	{
+		"name": "Spring Showdown",
+		"first": "Ivy",
+		"second": "Jack",
+		"third": "Karen",
+		"fourth": "Leo",
+		"date": "03/21/24"
+	},
+	{
+		"name": "Autumn Clash",
+		"first": "Mona",
+		"second": "Nate",
+		"third": "Olive",
+		"fourth": "Paul",
+		"date": "09/30/24"
+	}
+]`;
+	return JSON.parse(data);
+}
