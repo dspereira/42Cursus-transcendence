d275643d0c0364cf8ea824ae6f7b3e597e8d9360
Diogo13Antunes <diogoandrecandeias@gmail.com>
2024-08-09 14:53:02 +0100

    Refactoring in input validation midleware.

--- backend/transcendence/custom_utils/input_checker/InputChecker.py
+++ backend/transcendence/custom_utils/input_checker/InputChecker.py
@@ -4,9 +4,9 @@ import re
 class InputChecker():
 
 	def __init__(self) -> None:
-		self.allowed_tags = ['b', 'i', 'u', 'strong', 'em']
+		self.allowed_tags = ['b', 'i', 'u', 'strong', 'em', 'del']
 		self.valid_input = None
 
 	def get_valid_input(self, input_value):
-		self.valid_input = bleach.clean(input_value, tags=self.allowed_tags, strip=True)
+		self.valid_input = bleach.clean(input_value, tags=self.allowed_tags, strip=False)
 		return self.valid_input
