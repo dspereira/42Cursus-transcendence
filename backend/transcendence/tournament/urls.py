d2f8e64e4d731226222281976af6d75db5a9a8b1
dspereira <diogo.pereira.ee@gmail.com>
2024-08-14 15:14:56 +0100

    Update urls of tournaments

--- backend/transcendence/tournament/urls.py
+++ backend/transcendence/tournament/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
 	path('', TournamentView.as_view(), name='tournament'),
 	path('invite/', TournamentInvitesView.as_view(), name='invite'),
-    path('is-tournament-owner/', views.is_tournament_owner, name='is_tournament_owner'),
-    path('tournament-status/', views.get_tournament_state, name='get_tournament_state'),
-    path('tournament-players/', views.get_tournament_players, name='get_tournament_players'),
-    path('active-tournament/', views.active_tournament, name='active_tournament_'),
+    path('is-owner/', views.is_tournament_owner, name='is_tournament_owner'),
+    path('status/', views.get_tournament_state, name='get_tournament_state'),
+    path('players/', views.get_tournament_players, name='get_tournament_players'),
+    path('active-tournament/', views.active_tournament, name='active_tournament'),
 ]
