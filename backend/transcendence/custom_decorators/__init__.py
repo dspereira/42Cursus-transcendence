9224a5290df2de343dbe6683ca1e285262044bda
Diogo13Antunes <diogoandrecandeias@gmail.com>
2024-10-03 16:18:06 +0100

    Created decorator to validate the request body.

--- backend/transcendence/custom_decorators/__init__.py
+++ backend/transcendence/custom_decorators/__init__.py
@@ -1,3 +1,4 @@
 from .login_required import login_required
 from .accepted_methods import accepted_methods
 from .tfa_required import tfa_required
+from .check_request_body import check_request_body
