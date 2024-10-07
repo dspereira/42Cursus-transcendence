e3b13adc01d5e52f1f8cc2cfe4acf8f9ce65283f
Diogo13Antunes <diogoandrecandeias@gmail.com>
2024-10-07 15:33:15 +0100

    Resolved a bug and enhanced the body checker feature.

--- backend/transcendence/custom_decorators/check_request_body.py
+++ backend/transcendence/custom_decorators/check_request_body.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django import forms
 import json
 import sys
 
@@ -6,17 +7,35 @@ SUCCESS = "\033[1m\033[32m"
 ERROR = "\033[1m\033[31m"
 RESET = "\033[0m"
 
+AVAILABLE_CONTENT_TYPES = ['application/json', 'multipart/form-data']
+
+class UploadedForm(forms.Form):
+    pass
+
 def check_request_body(body_objects: list[str] = None):
 	def decorator(func):
 		def wrapper(request, *args, **kwargs):
-			if request.content_type == 'application/json':
-				loaded_body = __get_loaded_body(request.body)
-				if not loaded_body:
-					print(f'{ERROR}Failed to load JSON body.{RESET}')
-					return JsonResponse({"message": "Invalid body."}, status=400)
-				if not __exist_objects_in_body(body_objects, loaded_body):
-					print(f'{ERROR}Missing Objects in Body{RESET}')
-					return JsonResponse({"message": "Invalid body."}, status=400)
+			
+			if request.body:
+				content_type = request.content_type
+				if content_type in AVAILABLE_CONTENT_TYPES:
+					if content_type == 'application/json':
+						loaded_body = __get_loaded_body(request.body)
+						if not loaded_body:
+							print(f'{ERROR}Failed to load JSON body.{RESET}')
+							return JsonResponse({"message": "Invalid body."}, status=400)
+						if not __exist_objects_in_body(body_objects, loaded_body):
+							print(f'{ERROR}Missing Objects in Body{RESET}')
+							return JsonResponse({"message": "Invalid body."}, status=400)
+					elif content_type == 'multipart/form-data':
+						if request.FILES:
+							uploaded_form = UploadedForm(request.POST, request.FILES)
+							if not uploaded_form.is_valid():
+								print(f'{ERROR}Invalid form data.{RESET}')
+								return JsonResponse({"message": "Invalid body."}, status=400)
+				else:
+					print(f'{ERROR}Not Supported Content Type{RESET}')
+					return JsonResponse({"message": "Invalid body."}, status=415)
 			print(f'{SUCCESS}Body checked with SUCCESS.{RESET}')
 			return func(request, *args, **kwargs)
 		return wrapper
