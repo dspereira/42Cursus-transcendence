3b2054ff49c7e5402208869359f5d6c27e1cb28a
Diogo13Antunes <diogoandrecandeias@gmail.com>
2024-10-08 12:49:43 +0100

    Removed unecessary notifications app code.

--- backend/transcendence/notifications/views.py
+++ backend/transcendence/notifications/views.py
@@ -1,19 +1,12 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from custom_decorators import login_required, accepted_methods
 from custom_utils.models_utils import ModelManager
-
-from .models import FriendsRequestNotification
 from user_auth.models import User
-
 from game.utils import get_game_requests_list
 from tournament.utils import get_tournament_requests_list
 from friendships.friendships import get_friends_request_list
 
-import json
-
 user_model = ModelManager(User)
-friend_request_notification_model = ModelManager(FriendsRequestNotification)
 
 @login_required
 @accepted_methods(["GET"])
@@ -32,19 +25,3 @@ def requests_notifications(request):
 			status=200)
 	else:
 		return JsonResponse({"message": "Error: Invalid User!"}, status=400)
-
-@accepted_methods(["POST"])
-@login_required
-def create_friend_notification(request):
-	
-	if request.access_data:
-		user = user_model.get(id=request.access_data.sub)
-	
-	if user:
-		if request.body:
-			req_data = json.loads(request.body.decode('utf-8'))
-
-			print(req_data)
-
-	else:
-		return JsonResponse({"message": "User does not exist!"})
