6ca550977abb9315f60327b7f240064589cd76a0
Diogo13Antunes <diogoandrecandeias@gmail.com>
2024-10-10 12:01:22 +0100

    Updated script to clear all dockers stuff.

--- remove_all_dockers.sh
+++ remove_all_dockers.sh
@@ -1,5 +1,5 @@
-sudo docker stop $(sudo docker ps -aq)
-sudo docker rm $(sudo docker ps -aq)
-sudo docker rmi $(sudo docker images -q)
-sudo docker volume rm $(sudo docker volume ls -q)
-sudo docker network rm $(sudo docker network ls -q)
+sudo docker stop $(sudo docker ps -aq) 2>/dev/null
+sudo docker rm $(sudo docker ps -aq) 2>/dev/null
+sudo docker rmi $(sudo docker images -q) 2>/dev/null
+sudo docker volume rm $(sudo docker volume ls -q) 2>/dev/null
+sudo docker network rm $(sudo docker network ls -q) 2>/dev/null
