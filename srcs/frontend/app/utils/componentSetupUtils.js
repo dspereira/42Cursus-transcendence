bb5ff03f538e69320d535da3f88cec05812d60b6
dspereira <diogo.pereira.ee@gmail.com>
2024-10-11 17:14:31 +0100

    Added event to deal with online/offline browser status.

--- srcs/frontend/app/utils/componentSetupUtils.js
+++ srcs/frontend/app/utils/componentSetupUtils.js
@@ -1,6 +1,8 @@
+import stateManager from "../js/StateManager.js";
 import onerrorEventImg from "./imageErrorUtil.js";
 
 const componentSetup = function(component, html, styles) {
+	const isPage = isCompPage(`${component.outerHTML}`);
 	const compHtml = document.createElement("div");
 	compHtml.innerHTML = html;
 	if (styles) {
@@ -12,7 +14,44 @@ const componentSetup = function(component, html, styles) {
 	}
 	component.appendChild(compHtml);
 	onerrorEventImg(compHtml);
+	
+	if (isPage)
+		browserOnlineEvent(compHtml, online, offline);
+	
 	return compHtml;
+}
+
+const isCompPage = function(elm) {
+	if (elm.indexOf("page") == 1)
+		return true;
+	else false;
+}
+
+const browserOnlineEvent = function(html, callbackOnline, callbackOffline) {
+	stateManager.addEvent("isOnline", (status) => {
+		if (status)
+			callbackOnline(html);
+		else
+			callbackOffline(html);
+	});
+}
+
+const online = function(html) {
+
+	// Just for debug pls remove
+	console.log("online: ", html);
+	const elm = html.querySelector(".content");
+	elm.classList.add("back-red");
+	elm.classList.remove("back-blue");
+}
+
+const offline = function(html) {
+
+	// Just for debug pls remove
+	console.log("offline: ", html);
+	const elm = html.querySelector(".content");
+	elm.classList.add("back-blue");
+	elm.classList.remove("back-red");
 }
 
 export default componentSetup;
