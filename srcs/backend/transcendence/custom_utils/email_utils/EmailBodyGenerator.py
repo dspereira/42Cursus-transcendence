d0715f54e3ae348289b6b6aad2f901d073915ead
Diogo andre Candeias antunes <dcandeia@c2r1s7.42lisboa.com>
2024-10-14 14:21:12 +0100

    Fixed link inside validation email.

--- srcs/backend/transcendence/custom_utils/email_utils/EmailBodyGenerator.py
+++ srcs/backend/transcendence/custom_utils/email_utils/EmailBodyGenerator.py
@@ -3,7 +3,7 @@ from .html_body.email_verification import email_verification_html
 
 from custom_utils.jwt_utils import JwtData
 
-from transcendence.settings import ALLOWED_HOSTS
+from transcendence.settings import ALLOWED_HOSTS, ALLOWED_PORT
 
 class EmailBodyGenerator:
 	def get_verication_code(self, code: str):
@@ -11,6 +11,6 @@ class EmailBodyGenerator:
 		return result_html
 
 	def get_email_verication(self, token):
-		email_verify_url = f"https://{ALLOWED_HOSTS[0]}/email-verification/" + str(token)
+		email_verify_url = f"https://{ALLOWED_HOSTS[0]}:{str(ALLOWED_PORT)}/email-verification/" + str(token)
 		result_html = email_verification_html.format(email_verify_url=email_verify_url)
 		return result_html
