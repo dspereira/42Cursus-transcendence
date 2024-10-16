e237833fa4e323e8b6105c9f1ce255839012f828
Diogo13Antunes <diogoandrecandeias@gmail.com>
2024-10-16 09:55:43 +0100

    Updated API toute that checks if the server is or not alive.

--- srcs/backend/transcendence/transcendence/views.py
+++ srcs/backend/transcendence/transcendence/views.py
@@ -1,6 +1,6 @@
 from custom_decorators import accepted_methods
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 @accepted_methods(['GET'])
 def check(request):
-    return HttpResponse(status=200)
+	return JsonResponse({"message": f"API Server is Online!"}, status=200)
